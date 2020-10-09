977a84e938bbff8ba624b8bce3ab7bcf83f5946c
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-10-10 04:27:02 +1100

    add monogodb setup and database for Citadels

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const MongoClient = require("mongodb").MongoClient;
-const express = require("express");
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
 const app = express();
 
-
-
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
-console.log("Server running at Port: 3000");
+console.log('Server running at Port: 3000');
+
+//DATABASE MONGODB
+const uri =
+  'mongodb+srv://Hassan:SIT725@sit725.bketa.mongodb.net/Citadels(SIT725)?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect((err) => {
+  collection_userdetails = client.db('Citadels(SIT725)').collection('users');
+});
