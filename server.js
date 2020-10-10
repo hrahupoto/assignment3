1b794cad4feca34eb745d260e7403abe568c61ea
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-10-11 00:50:48 +1100

    - add citadels user collection - add end point for insert user, get users, removeusers

--- server.js
+++ server.js
@@ -4,17 +4,60 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/insertUser', function (req, res) {
+  if (req.query.userName == null || req.query.dateOfBirth == null) {
+    res.send('Enter user name and date of birth.');
+  } else {
+    insertUser({
+      userName: req.query.userName,
+      dateOfBirth: req.query.dateOfBirth,
+    });
+    res.send('User saved in database');
+  }
+});
+
+app.get('/getUsers', function (req, res) {
+  userCollection.find({}).toArray(function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(JSON.stringify(result));
+    }
+  });
+});
+
+app.get('/deleteAllUsers', function (req, res) {
+  userCollection.remove({}, (err, item) => {
+    if (err) {
+      return res.json({success: false, msg: 'Cannot remove users'});
+    }
+    if (!item) {
+      return res.status(404).json({success: false, msg: 'Users not found'});
+    }
+    res.json({success: true, msg: 'Users removed.'});
+  });
+});
+
 app.listen(3000);
 console.log('Server running at Port: 3000');
 
 //DATABASE MONGODB
+let userCollection;
+
 const uri =
   'mongodb+srv://Hassan:SIT725@sit725.bketa.mongodb.net/Citadels(SIT725)?retryWrites=true&w=majority';
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
+const insertUser = ({userName, dateOfBirth}) => {
+  userCollection.insertOne({
+    userName,
+    dateOfBirth,
+  });
+};
+
 client.connect((err) => {
-  collection_userdetails = client.db('Citadels(SIT725)').collection('users');
+  userCollection = client.db('Citadels(SIT725)').collection('users');
 });
