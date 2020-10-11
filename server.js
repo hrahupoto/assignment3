114df18c02599f937f2c70b398f5daf49fb8bb6f
ruwanganath ramanayake <ruwanganath@Samadi.localdomain>
2020-10-12 06:21:12 +1100

    Lobby page layout - Ruwan

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/lobby',function(req,res){
+    res.redirect('/lobby.html');
+})
+
 app.listen(3000);
 console.log("Server running at Port: 3000");
