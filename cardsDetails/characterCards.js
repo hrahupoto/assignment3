8a2a8761c83bedfa173c817b529085a9fbee2ca8
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-10-15 12:36:03 +1100

    added cards required details

---
+++ cardsDetails/characterCards.js
@@ -0,0 +1,13 @@
+var characterCards;
+
+characterCards = {
+  1: {location: '/images/characterCards/1Assassin.jpg'},
+  2: {location: '/images/characterCards/2Thief.jpg'},
+  3: {location: '/images/characterCards/3Magician.jpg'},
+  4: {location: '/images/characterCards/4King.jpg'},
+  5: {location: '/images/characterCards/5Bishop.jpg'},
+  6: {location: '/images/characterCards/6Merchant.jpg'},
+  7: {location: '/images/characterCards/7Architect.jpg'},
+  8: {location: '/images/characterCards/8Warload.jpg'},
+  9: {location: '/images/characterCards/9Artist.jpg'},
+};
