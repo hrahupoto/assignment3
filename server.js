0f3923bd7bbb7b1c45a70cb036f4d8ad16054986
sttalari <tejuts2000@gmail.com>
2020-10-15 19:12:04 +1100

    Modified code according to ejs

--- server.js
+++ server.js
@@ -15,14 +15,18 @@ app.use('/', getUsers);
 app.use('/', deleteAllUsers);
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');  
+app.set('view engine', 'ejs');
 
-app.get('/',function(req,res){
-    res.render('lobby', {title:"Citadels - Lobby"});
+app.get('/', function(req, res) {
+    res.render('lobby', { title: "Citadels - Lobby" });
 })
 
-app.get('/gameRoom',function(req,res){
-    res.render('gameRoom', {title:"Citadels - Game Room"});
+app.get('/help', function(req, res) {
+    res.render('help', { title: "Citadels - Help" });
+})
+
+app.get('/gameRoom', function(req, res) {
+    res.render('gameRoom', { title: "Citadels - Game Room" });
 })
 
 // app.get('/PlayerRoom',function(req,res){
@@ -35,19 +39,18 @@ console.log('Server running at Port: 3000');
 //DATABASE MONGODB
 
 const uri =
-  'mongodb+srv://Hassan:SIT725@sit725.bketa.mongodb.net/Citadels(SIT725)?retryWrites=true&w=majority';
+    'mongodb+srv://Hassan:SIT725@sit725.bketa.mongodb.net/Citadels(SIT725)?retryWrites=true&w=majority';
 
 mongoose.connect(
-  uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-  function (err) {
-    if (err) throw err;
+    uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    },
+    function(err) {
+        if (err) throw err;
 
-    console.log('DB successfully connected');
-  }
+        console.log('DB successfully connected');
+    }
 );
