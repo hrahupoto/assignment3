064cac2cee2f9370a2675ffeefa35ca8fbca2b16
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-10-23 19:21:52 +1100

    added socket on start game functionality for multiplayers

--- controllers/startGame.js
+++ controllers/startGame.js
@@ -1,80 +1,65 @@
 const userModel = require('../models/db/user');
-const { Player } = require('../models/player');
-const { districtCards } = require('../modules/districtCards');
-const { characterCards } = require('../modules/characterCards');
+const {Player} = require('../models/player');
+const {districtCards} = require('../modules/districtCards');
+const {characterCards} = require('../modules/characterCards');
 const Bank = require('../models/bank');
 var Initial_Coins = 2;
 
 var random_num = [];
 while (random_num.length < 47) {
-    var num = Math.floor(Math.random() * (48 - 1) + 1);
-    if (random_num.indexOf(num) === -1) random_num.push(num);
+  var num = Math.floor(Math.random() * (48 - 1) + 1);
+  if (random_num.indexOf(num) === -1) random_num.push(num);
 }
 
 //console.log(random_num)
 var remaining_dc = [];
 var playercards = [];
 for (var i = 0; i < 16; i++) {
-    playercards.push(districtCards[random_num[i]])
+  playercards.push(districtCards[random_num[i]]);
 }
 //console.log(playercards)
 
 for (var j = 16; j < 47; j++) {
-    remaining_dc.push(districtCards[random_num[j]]);
+  remaining_dc.push(districtCards[random_num[j]]);
 }
 //console.log(remaining_dc);
 
 var initial_dsc_cards = [];
-var i, j, temp_array, chunk = 4;
+var i,
+  j,
+  temp_array,
+  chunk = 4;
 for (i = 0, j = playercards.length; i < j; i += chunk) {
-    temp_array = playercards.slice(i, i + chunk);
-    initial_dsc_cards.push(temp_array);
+  temp_array = playercards.slice(i, i + chunk);
+  initial_dsc_cards.push(temp_array);
 }
 
-exports.startGame = function(req, res) {
-    var players = [];
-    var maxPlayer = 4;
-    var bal_coins;
-    var bank = [];
-    var bankcoins = 100;
-    var bankcomp;
-    //var remaining_bankcoins = 0;
-    //var total_Initial_Coins = 0;
-    userModel.find({}, function(err, users) {
-        if (users.length == maxPlayer) {
-            //console.log(districtCards.length)
-            /*for (var i = 0; i < 47; i++) {
-                bank.push(new Bank(bankcoins, districtCards[i]));
-                //dcards.push(districtCards[i]);
-            }*/
-            //console.log(playercards)
+exports.startGame = function (req, res) {
+  var players = [];
+  var maxPlayer = 4;
+  var bal_coins;
+  var bank = [];
+  var bankcoins = 100;
+  userModel.find({}, function (err, users) {
+    if (users.length == maxPlayer) {
+      bank = new Bank(bankcoins);
 
-            bank = (new Bank(bankcoins));
-
-            //console.log(bank[0].dcsArray);
-            //console.log(bank);
-            //bank.push(new Bank(bankcoins, districtCards, characterCards));
-            //console.log(bank[0].coins);
-            //bank_coins = bank[0].coins;
-            //result = bank[0].withdrawFromBank(Initial_Coins);
-            //console.log("Result:", result)
-            //console.log(bank.withdrawFromBank(Initial_Coins));
-            //withdraw.push(Bank.withdrawFromBank);
-            //console.log(withdraw);
-
-            for (var i = 0; i < users.length; i++) {
-                players.push(new Player(i, users[i].userName, users[i].dateOfBirth, Initial_Coins, initial_dsc_cards[i]));
-                bal_coins = bank.withdrawFromBank(Initial_Coins);
-                //bankcoins -= Initial_Coins;
-            }
-            //console.log(bal_coins);
-            bank = (new Bank(bal_coins, remaining_dc, characterCards));
-            //console.log(bank.dcsArray[0]);
-            //bank_coins = bank[bank.length - 1].coins;
-            //console.log(bank_coins);
-            return res.json({ players, bank });
-        } else {
-            return res.json('Please wait for more players to show up.');
-        }
-    });
+      for (var i = 0; i < users.length; i++) {
+        players.push(
+          new Player(
+            i,
+            users[i].userName,
+            users[i].dateOfBirth,
+            Initial_Coins,
+            initial_dsc_cards[i]
+          )
+        );
+        bal_coins = bank.withdrawFromBank(Initial_Coins);
+      }
+      bank = new Bank(bal_coins, remaining_dc, characterCards);
+      return res.json({players, bank});
+    } else {
+      return res.json('Please wait for more players to show up.');
+    }
+  });
 };
--- public/env.js
+++ public/env.js
@@ -2,194 +2,195 @@
 const socket = io.connect('http://localhost:3000');
 var counter;
 
-$(document).ready(function() {
+$(document).ready(function () {
+  //login functionality
+  $('.joinRoom').click(function () {
+    var userName = $('#userName').val();
+    var dateOfBirth = $('#dob').val();
 
-    //login functionality
-    $('.joinRoom').click(function() {
-        var userName = $('#userName').val();
-        var dateOfBirth = $('#dob').val();
-
-        $.ajax({
-            type: 'GET',
-            url: '/insertUser',
-            data: { userName, dateOfBirth },
-            success: function(data) {
-                if (
-                    data == 'User already exists, Please try entering different username.'
-                ) {
-                    alert(JSON.stringify(data));
-                } else if (data == 'Game room is full. Please try again later.') {
-                    alert(JSON.stringify(data));
-                } else {
-                    window.location.href = 'gameRoom';
-                }
-            },
-            error: function() {},
-        });
-        console.log(userName);
-        console.log(dateOfBirth);
+    $.ajax({
+      type: 'GET',
+      url: '/insertUser',
+      data: {userName, dateOfBirth},
+      success: function (data) {
+        if (
+          data == 'User already exists, Please try entering different username.'
+        ) {
+          alert(JSON.stringify(data));
+        } else if (data == 'Game room is full. Please try again later.') {
+          alert(JSON.stringify(data));
+        } else {
+          window.location.href = 'gameRoom';
+        }
+      },
+      error: function () {},
     });
+    console.log(userName);
+    console.log(dateOfBirth);
+  });
 
-    // start game functionality
-    $('a[href="#startGame"]').click(function() {
-        $.ajax({
+  // start game functionality
+  $('a[href="#startGame"]').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/startGame',
+      data: {},
+      success: function (players) {
+        if (players == 'Please wait for more players to show up.') {
+          alert(players);
+        } else {
+          //using socket for start game if any one of the player
+          //presses start game it should start the game for all the players.
+          socket.emit('startGame', {
+            players: players,
+          });
+        }
+      },
+      error: function () {},
+    });
+  });
+  //exit function functionality
+  $('.exitGame').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/deleteAllUsers', //delete all users before redirecting to lobby
+      data: {},
+      success: function () {},
+      error: function () {},
+    });
+    window.location.href = '/';
+  });
+
+  $('.collapsible').click(function () {
+    var content = document.getElementById('content');
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+
+  //Emit events
+  $('#send').click(function () {
+    var message = $('#message').val();
+    var playerName = $('#handle').val();
+    socket.emit('chat', {
+      message: message,
+      playerName: playerName,
+    });
+    $('#message').val('');
+  });
+
+  $('#message').keypress(function () {
+    var playerName = $('#handle').val();
+    socket.emit('typing', playerName);
+  });
+
+  // Listen for chat events
+  socket.on('chat', function (data) {
+    feedback.innerHTML = '';
+    output.innerHTML +=
+      '<p><strong>' + data.playerName + ': </strong>' + data.message + '</p>';
+  });
+
+  // listen for typing events
+  socket.on('typing', function (data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+  });
+});
+
+$('.game-Room').ready(function () {
+  document.getElementById('timer').innerHTML = 02 + ':' + 00;
+  counter = setInterval(() => {
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    $.ajax({
+      type: 'GET',
+      url: '/userCounter',
+      data: {timeArray},
+      success: function (data) {
+        //console.log(data);
+        if (data == 'hide') {
+          $.ajax({
             type: 'GET',
-            url: '/startGame',
+            url: '/startGame', //start the game if 4 users are present in the room
             data: {},
-            success: function(players) {
-                if (players == 'Please wait for more players to show up.') {
-                    alert(players);
-                } else {
-                    $('.startGame').hide(); //hide start game button after game is started
-                    clearInterval(counter);
-                    $('#Counter').hide();
-                    var bank_coins = players.bank.coins;
-                    $('.bank_coins').append(`<a>
-                    <img class="coins" src="/images/bank/coins.png">
-                    <div class="coins">${bank_coins}</div></a>`);
-                    console.log(players);
-                    for (i = 0; i < players.players.length; i++) {
-                        var earned_player_coins = players.players[i].coins;
-                        $('.players_coins').append(`<a>
-          <img class="player${i}_coins" src="/images/bank/coins.png">
-          <div class="player${i}_coins">${earned_player_coins}</div></a>`);
-                    }
-                }
+            success: function (players) {
+              socket.emit('startGame', {
+                players: players,
+              });
             },
-            error: function() {},
-        });
-    });
-    //exit function functionality
-    $('.exitGame').click(function() {
-        $.ajax({
+            error: function () {},
+          });
+          //document.getElementsById('Counter').style.visibility = 'hidden';
+        } else if (data == 'redirect') {
+          clearInterval(counter);
+          $.ajax({
             type: 'GET',
             url: '/deleteAllUsers', //delete all users before redirecting to lobby
             data: {},
-            success: function() {},
-            error: function() {},
-        });
-        window.location.href = '/';
-    });
-
-    $('.collapsible').click(function() {
-        var content = document.getElementById("content")
-        if (content.style.display === "block") {
-            content.style.display = "none";
+            success: function () {},
+            error: function () {},
+          });
+          window.location.href = '/';
         } else {
-            content.style.display = "block";
+          //socket emiting event for start game
+          socket.emit('timer', {
+            socketID: socket.id,
+            minutes: data.minutes,
+            seconds: data.seconds,
+          });
         }
+      },
+      error: function () {},
     });
-
-    //Emit events
-    $('#send').click(function() {
-        var message = $('#message').val()
-        var playerName = $('#handle').val()
-        socket.emit('chat', {
-            message: message,
-            playerName: playerName
-        });
-        $('#message').val('');
-    });
-
-    $('#message').keypress(function() {
-        var playerName = $('#handle').val()
-        socket.emit('typing', playerName);
-    })
-
-    // Listen for chat events
-    socket.on('chat', function(data) {
-        feedback.innerHTML = '';
-        output.innerHTML += '<p><strong>' + data.playerName + ': </strong>' + data.message + '</p>';
-    });
-
-    // listen for typing events
-    socket.on('typing', function(data) {
-        feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
-    });
-});
-
-$('.game-Room').ready(function() {
-    document.getElementById('timer').innerHTML = 02 + ':' + 00;
-    counter = setInterval(() => {
-        var presentTime = document.getElementById('timer').innerHTML;
-        var timeArray = presentTime.split(/[:]+/);
-        $.ajax({
-            type: 'GET',
-            url: '/userCounter',
-            data: { timeArray },
-            success: function(data) {
-                //console.log(data);
-                if (data == 'hide') {
-                    clearInterval(counter);
-                    $('#Counter').hide();
-                    $('.startGame').hide();
-                    $.ajax({
-                        type: 'GET',
-                        url: '/startGame', //start the game if 4 users are present in the room
-                        data: {},
-                        success: function(players) {
-                            var bank_coins = players.bank.coins;
-                            $('.bank_coins').append(`<a>
-                    <img class="coins" src="/images/bank/coins.png">
-                    <div class="coins">${bank_coins}</div></a>`);
-                            for (i = 0; i < players.players.length; i++) {
-                                var earned_player_coins = players.players[i].coins;
-                                $('.players_coins').append(`<a>
-              <img class="player${i}_coins" src="/images/bank/coins.png">
-              <div class="player${i}_coins">${earned_player_coins}</div></a>`);
-                            }
-                        },
-                        error: function() {},
-                    });
-                    //document.getElementsById('Counter').style.visibility = 'hidden';
-                } else if (data == 'redirect') {
-                    clearInterval(counter);
-                    $.ajax({
-                        type: 'GET',
-                        url: '/deleteAllUsers', //delete all users before redirecting to lobby
-                        data: {},
-                        success: function() {},
-                        error: function() {},
-                    });
-                    window.location.href = '/';
-                } else {
-                    //scoket emiting event
-                    socket.emit('timer', {
-                        socketID: socket.id,
-                        minutes: data.minutes,
-                        seconds: data.seconds,
-                    });
-                }
-            },
-            error: function() {},
-        });
-    }, 1000);
+  }, 1000);
 });
 
 //socket listening event
 socket.on('timer', (timer) => {
-    //console.log(timer);
-    document.getElementById('timer').innerHTML =
-        timer.minutes + ':' + timer.seconds;
+  //console.log(timer);
+  document.getElementById('timer').innerHTML =
+    timer.minutes + ':' + timer.seconds;
 });
 
-$('.game-room').ready(function() {
-    var appendPlayers = setInterval(() => {
-        $.ajax({
-            type: 'GET',
-            url: '/getUsers',
-            data: {},
-            success: function(players) {
-                for (var i = 0; i < players.users.length; i++) {
-                    var num = players.users[i].num;
-                    var userName = players.users[i].userName;
-                    $('.players').append(`<div class="player${num}">${userName}</div>`);
-                }
-                if (players.users.length == 4) {
-                    clearInterval(appendPlayers);
-                }
-            },
-            error: function() {},
-        });
-    }, 1000);
+socket.on('startGame', (players) => {
+  //using socket for start game if any one of the player
+  //presses start game it should start the game for all the players.
+  console.log(players);
+  $('.startGame').hide(); //hide start game button after game is started
+  clearInterval(counter);
+  $('#Counter').hide();
+  var bank_coins = players.bank.coins;
+  $('.bank_coins').append(`<a>
+    <img class="coins" src="/images/bank/coins.png">
+    <div class="coins">${bank_coins}</div></a>`);
+
+  for (i = 0; i < players.players.length; i++) {
+    var earned_player_coins = players.players[i].coins;
+    $('.players_coins').append(`<a>
+<img class="player${i}_coins" src="/images/bank/coins.png">
+<div class="player${i}_coins">${earned_player_coins}</div></a>`);
+  }
+});
+
+$('.game-room').ready(function () {
+  var appendPlayers = setInterval(() => {
+    $.ajax({
+      type: 'GET',
+      url: '/getUsers',
+      data: {},
+      success: function (players) {
+        for (var i = 0; i < players.users.length; i++) {
+          var num = players.users[i].num;
+          var userName = players.users[i].userName;
+          $('.players').append(`<div class="player${num}">${userName}</div>`);
+        }
+        if (players.users.length == 4) {
+          clearInterval(appendPlayers);
+        }
+      },
+      error: function () {},
+    });
+  }, 1000);
 });
