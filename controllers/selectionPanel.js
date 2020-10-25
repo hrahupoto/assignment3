a796f91009c3ae7a2a618e77af8c4ea8668e0609
Mashal <mashal_valliani@hotmail.com>
2020-10-26 00:02:04 +1100

    selectionPanel1 completed

--- controllers/selectionPanel.js
+++ controllers/selectionPanel.js
@@ -1,34 +1,51 @@
-const {characterCards} = require('../modules/characterCards');
+const {characterCards}  = require("../modules/characterCards");
 var cCards = [1,2,3,4,5,6,7,8,9]
-
+var ccSelected = [];
 exports.selectionPanel = function (req, res) {
-    function stepOne(){
-        var faceUpCards = [];
-        var faceDownCards = [];
-        var playerCharcCards = [];
-    if(faceUpCards.length<4){
-        var randNumber = Math.floor(Math.random() * (9 - 1) + 1);
-        if (randNumber != 4){
-            if(faceUpCards.indexOf(randNumber) === -1) {
-                faceUpCards.push(randNumber);
-                cCards.splice(randNumber,1)
-                //send rand number to env
-            }
-        else
-        randNumber = Math.floor(Math.random() * (9 - 1) + 1);
-        }
+  var clickCount = req.query.clickCount;
+  //console.log(cCards[6].location);
+  //   var faceUpCards = [];
+  //     var faceDownCards = [];
+  //   var playerCharcCards = [];
+  if (clickCount < 3) {
+    var randNumber = Math.floor(Math.random() * cCards.length);
+    randNumber = cCards[randNumber];
+    console.log("randnumber: " + randNumber);
+    while (randNumber == 4) {
+        var randNumber = Math.floor(Math.random() * cCards.length);
+        randNumber = cCards[randNumber];
+      console.log("randnumber: " + randNumber);
     }
-    if(faceUpCards.length==3 && faceDownCards.length<1){
-        var randNumber = Math.floor(Math.random() * (9 - 1) + 1);
-        if (randNumber != 4){
-            if(faceUpCards.indexOf(randNumber) === -1) {
-                faceUpCards.push(randNumber);
-                cCards.splice(randNumber,1)
-                //send rand number to env
-            }
-        else
-        randNumber = Math.floor(Math.random() * (9 - 1) + 1);
+    if (randNumber != 4) {
+      if (ccSelected.indexOf(randNumber) === -1) {
+        ccSelected.push(randNumber);
+        if(randNumber>-1){
+            var index = cCards.indexOf(randNumber)
+        cCards.splice(index, 1);
+        console.log("cCards: " + cCards);
         }
+        //     send rand number to env
+        var cardLoc = characterCards[randNumber].location;
+       // console.log(characterCards[randNumber].location);
+        return res.send({cardLoc,cCards});
+      }
     }
-}
-}
+  }
+  if (clickCount == 3) {
+    var randNumber = Math.floor(Math.random() * cCards.length);
+    randNumber = cCards[randNumber];
+    console.log("randnumber: " + randNumber);
+    if (ccSelected.indexOf(randNumber) === -1) {
+        ccSelected.push(randNumber);
+        if(randNumber>-1){
+            var index = cCards.indexOf(randNumber)
+        cCards.splice(index, 1);
+        console.log("cCards: " + cCards);
+        }
+        //     send rand number to env
+        var cardLoc = characterCards[randNumber].location;
+        console.log(characterCards[randNumber].location);
+        return res.send({cardLoc,cCards});
+      }
+  }
+};
--- public/env.js
+++ public/env.js
@@ -1,6 +1,7 @@
 //Make connection
 const socket = io.connect("http://localhost:3000");
 var counter;
+var clickCount = 0;
 
 $(document).ready(function () {
   //login functionality
@@ -58,77 +59,317 @@ $(document).ready(function () {
                 .append(`<div class="player${i}Crown" id="player${i}Crown">
               <img src="/images/bank/crown.png" />
             </div>`);
-            //User Game Turn Pointer
-            var $pointer = $(`.player${i}Pointer`);
-            $pointer.css("visibility", "visible");
+              //User Game Turn Pointer
+              var $pointer = $(`.player${i}Pointer`);
+              $pointer.css("visibility", "visible");
             }
-            $("#crown_disapear").hide();
-            //Game Starts: Selection Panel for character Cards
-            // 3 seconds delay before game starts
-            setTimeout(function () {
-              //Displaying the first selection panel
-              $("#selectionPanel").show();
-            }, 1500);
-            $("#cc1").click(function () {
-              $("#cc1").hide();
-              if (clickCount == 0) {
-                $("#ccFaceUp1").attr("src", "location");
-                clickCount = clickCount + 1;
-              }
-              if (clickCount == 1) {
-                $("#ccFaceUp2").attr("src", "location");
-                clickCount = clickCount + 1;
-              }
-              if (clickCount == 2) {
-                $("#ccFaceUp3").attr("src", "location");
-                clickCount = clickCount + 1;
-              }
-              if (clickCount == 3) {
-                $("#ccFaceDown").attr("src", "location");
-                clickCount = clickCount + 1;
-              }
-            });
-            $("#cc2").click(function () {
-              $("#cc2").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc3").click(function () {
-              $("#cc3").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc4").click(function () {
-              $("#cc4").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc5").click(function () {
-              $("#cc5").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc6").click(function () {
-              $("#cc6").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc7").click(function () {
-              $("#cc7").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc8").click(function () {
-              $("#cc8").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
-            $("#cc9").click(function () {
-              $("#cc9").hide();
-              $("#ccFaceUp1").attr("src", "location");
-              clickCount = clickCount + 1;
-            });
           }
+          $("#crown_disapear").hide();
+          //Game Starts: Selection Panel for character Cards
+          // 3 seconds delay before game starts
+          setTimeout(function () {
+            //Displaying the first selection panel
+            $("#selectionPanel").show();
+            }, 1500);
+          $("#cc1").click(function () {
+            $("#cc1").hide();
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc1").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc1").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc1").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc2").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc2").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc2").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc2").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc2").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 4){
+                  alert("Cards already Selected");
+                  }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc3").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc3").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc3").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc3").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc3").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc4").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc4").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc4").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc4").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc4").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc5").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc5").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc5").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc5").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc5").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc6").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc6").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc6").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc6").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc6").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc7").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc7").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc7").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc7").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc7").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc8").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc8").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc8").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc8").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc8").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#cc9").click(function () {
+            $.ajax({
+              type: "GET",
+              url: "/selectionPanel",
+              data: { clickCount },
+              success: function (data) {
+                console.log(data.cardLoc);
+                console.log(data);
+                if (clickCount == 0) {
+                  $("#cc9").hide();
+                  $("#ccFaceUp1").append(`<img id="ccFaceUp1" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 1) {
+                  $("#cc9").hide();
+                  $("#ccFaceUp2").append(`<img id="ccFaceUp2" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 2) {
+                  $("#cc9").hide();
+                  $("#ccFaceUp3").append(`<img id="ccFaceUp3" src='${data.cardLoc}'>`);
+                  clickCount = clickCount + 1;
+                }
+                else if (clickCount == 3) {
+                  $("#cc9").hide();
+                  $("#ccFacedown").append(`<img id="ccFacedown" src="/images/characterCards/CharacterCard_rear.jpg">`);
+                  clickCount = clickCount + 1;
+                }
+              },
+              error: function () {},
+            });
+          });
+          $("#ccPanelBtn").click(function () {
+            if(clickCount==4){
+              $("#selectionPanel2").show();
+              $("#selectionPanel").hide();
+            }
+            else
+            alert("Error: Please select all cards.")
+          });
         }
       },
       error: function () {},
@@ -187,6 +428,7 @@ $(document).ready(function () {
 $(".game-Room").ready(function () {
   //hiding Selection panel Initially
   $("#selectionPanel").hide();
+  $("#selectionPanel2").hide();
   document.getElementById("timer").innerHTML = 02 + ":" + 00;
   counter = setInterval(() => {
     var presentTime = document.getElementById("timer").innerHTML;
